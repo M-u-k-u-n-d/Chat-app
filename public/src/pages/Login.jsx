ee4e8dea1d0db46cd010e943c0c1991451155041
Mukund Tiwari <mukundtiwari251103@gmail.com>
2024-01-20 17:13:05 +0530

    some updates

--- public/src/pages/Login.jsx
+++ public/src/pages/Login.jsx
@@ -21,7 +21,7 @@ export default function Login() {
     if (localStorage.getItem(process.env.REACT_APP_LOCALHOST_KEY)) {
       navigate("/");
     }
-  }, []);
+  });
 
   const handleChange = (event) => {
     setValues({ ...values, [event.target.name]: event.target.value });
